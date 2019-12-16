f720f3f097f3639ece515a5ee983070eb2dacc66
Rhys Maiden <maidenrhys@gmail.com>
2019-12-16 14:51:28 +0700

    Removed all auth from url.py

--- rugby/urls.py
+++ rugby/urls.py
@@ -35,7 +35,6 @@ urlpatterns = [
     #     MatchView.as_view(template_name="rugby/home.html")),
     # url(r'^league/(?P<pk>[0-9]+)/$',
     #     LeagueView.as_view(template_name="rugby/home.html")),
-    url(r'^accounts/', include('allauth.urls')),
     url(r'^ratings/', include('star_ratings.urls', namespace='ratings')),
     url(r'^highlights/$', views.Highlights.as_view()),
     url(r'^player/$', views.PlayerAPI.as_view()),
