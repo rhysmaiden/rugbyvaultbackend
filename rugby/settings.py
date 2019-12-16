eac0bccd41d2e1e8a234027d78c1766cf5d9d43b
Rhys Maiden <maidenrhys@gmail.com>
2019-12-16 14:52:46 +0700

    really removed this time

--- rugby/settings.py
+++ rugby/settings.py
@@ -82,15 +82,6 @@ TEMPLATES = [
     },
 ]
 
-AUTHENTICATION_BACKENDS = (
-
-    # Needed to login by username in Django admin, regardless of `allauth`
-    'django.contrib.auth.backends.ModelBackend',
-
-    # `allauth` specific authentication methods, such as login by e-mail
-    'allauth.account.auth_backends.AuthenticationBackend',
-
-)
 
 WSGI_APPLICATION = 'rugby.wsgi.application'
 
