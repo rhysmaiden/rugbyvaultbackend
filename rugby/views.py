1f0a8bf17823b4af5aaf7a6b3ac90bee458d5713
rmai2995 <rmai2995@sydney.uni.edu.au>
2019-12-25 20:08:51 +1100

    Try processing

--- rugby/views.py
+++ rugby/views.py
@@ -492,8 +492,10 @@ class TryProcessingAPI(APIView):
        
         match_id = request.GET.get('id')
 
+        league = League.objects.filter(name="Super Rugby")[0]
+
         if match_id == 'undefined':
-            match_object = Match.objects.filter(match_completely_processed=0,video_link_found=1,error=0).order_by('-date')[0]
+            match_object = Match.objects.filter(match_completely_processed=0,video_link_found=1,error=0,league_id=league).order_by('-date')[0]
         else:
             match_object = Match.objects.filter(id=match_id)[0]
 
